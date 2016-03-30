76668583c2ad1d06ac64985b99fa3b47f9a6871e
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-03-30 10:34:39 +0200

    Enhanced user model to create worker after user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   # Do not forget to add only: [:action] if necessary
   before_action :set_user
+  accepts_nested_attributes_for :worker
+  after_create  :create_worker
 
   def show
   end
@@ -17,9 +19,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create_worker
+   user_worker = Worker.new(user: self)
+   user_worker.save!(validate: false)
+  end
+
   # Do not forget to update the strong params here under
   # if you want
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :avatar, :provider)
+    params.require(:user).permit(:first_name, :last_name, :email, :avatar, :provider, worker_attributes: [:working_style])
   end
+
+
 end

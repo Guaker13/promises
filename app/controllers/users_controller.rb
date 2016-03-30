6fb75a90fa67b3aa48975f1730523ca06da431c5
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-03-30 10:57:35 +0200

    Created profile view functionality, with option to edit profile only for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   # Do not forget to add only: [:action] if necessary
   before_action :set_user
-  accepts_nested_attributes_for :worker
-  after_create  :create_worker
+
 
   def show
   end
@@ -17,11 +16,6 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def create_worker
-   user_worker = Worker.new(user: self)
-   user_worker.save!(validate: false)
   end
 
   # Do not forget to update the strong params here under

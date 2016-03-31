0daa2728dc9628bc3a3370763ba0583e6ca7a5b8
gauthier fraiture <gauthier.fraiture@gmail.com>
2016-03-31 14:00:56 +0200

    able to add some unavailabilities now

--- app/controllers/unavailabilities_controller.rb
+++ app/controllers/unavailabilities_controller.rb
@@ -1,8 +1,9 @@
 class UnavailabilitiesController < ApplicationController
-  before_action :find_workspace#, only: [:show]
+  before_action :find_workspace
 
   def index
     @unavailabilities = @workspace.unavailabilities.all
+    @unavailability = @workspace.unavailabilities.build
   end
 
   def show
@@ -12,6 +13,19 @@ class UnavailabilitiesController < ApplicationController
   end
 
   def create
+    @unavailabilty = Unavailability.new(unavailability_params)
+    @unavailabilty.workspace = @workspace
+
+    respond_to do |format|
+      if @unavailabilty.save
+        format.html { redirect_to workspace_unavailabilities_path(@workspace), notice: 'Unavailability was successfully created.' }
+        format.json { render :show, status: :created, location: @unavailability }
+      else
+        @unavailabilities = Unavailability.all
+        format.html { render :index }
+        format.json { render json: @unavailability.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -26,10 +40,14 @@ class UnavailabilitiesController < ApplicationController
   private
 
   def set_unavailability
-    @unavailabilty = Unavailability.find(params[:id])
+    @unavailabilty = @workspace.Unavailability.find(params[:id])
   end
 
   def find_workspace
     @workspace = Workspace.find(params[:workspace_id])
+  end
+
+  def unavailability_params
+    params.require(:unavailability).permit(:workspace_id, :start_time, :end_time)
   end
 end
